bfe7c5e9d01639d680fb5812fa2e69c439712c60
Liam Vinberg <vinberg.liam@gmail.com>
2025-08-28 20:32:46 +0200

    add mini align, usage: ga + <key>

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -7,24 +7,21 @@ return {
 		require("mini.surround").setup()
 		require("mini.move").setup({
 			mappings = {
-				left = "<M-Left>",
-				right = "<M-Right>",
-				down = "<M-Down>",
-				up = "<M-Up>",
+				left       = "<M-Left>",
+				right      = "<M-Right>",
+				down       = "<M-Down>",
+				up         = "<M-Up>",
 
-				line_left = "<M-Left>",
+				line_left  = "<M-Left>",
 				line_right = "<M-Right>",
-				line_down = "<M-Down>",
-				line_up = "<M-Up>",
+				line_down  = "<M-Down>",
+				line_up    = "<M-Up>",
 			},
 		})
 
-		local copper = "#c98452"
-		local starlight = "#cfd8e3"
-		local bg = "#0f1316" -- slightly deeper than editor bg
-		local dim = "#5c6675"
-
 		require("mini.statusline").setup()
+        require("mini.align").setup()
+
 
 		vim.cmd([[highlight StatusLine guibg=NONE ctermbg=NONE]])
 		vim.cmd([[highlight StatusLineNC guibg=NONE ctermbg=NONE]])
