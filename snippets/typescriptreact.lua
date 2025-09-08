a1b0e32db0d1ddf7b30217d0c8322bfeb4f927a0
Liam Vinberg <vinberg.liam@gmail.com>
2025-09-08 17:34:29 +0200

    update typescriptreact snippet to rename component to file name

--- snippets/typescriptreact.lua
+++ snippets/typescriptreact.lua
@@ -2,6 +2,8 @@ local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
+local d = ls.dynamic_node
+local sn = ls.snippet_node
 
 return {
 	-- Next.js page component
@@ -14,14 +16,25 @@ return {
 
 	-- Next.js component with props
 	s("nxc", {
-		t("export function Component(){"),
+		t("export function "),
+		d(1, function(args)
+			local filename = vim.fn.expand('%:t:r')
+			local component_name = ""
+			for word in filename:gmatch("([^%-]+)") do
+				component_name = component_name .. word:sub(1,1):upper() .. word:sub(2)
+			end
+			return sn(nil, { i(1, component_name) })
+		end, {}),
+		t("(){"),
 		t({ "", "    return <div>" }),
-		i(1),
+		i(2),
 		t({ "</div>", "}" }),
 	}),
 
 	s("nxl", {
-		t("export default function Layout({children}: {children: React.ReactNode}){"),
+		t("export default function "),
+		i(1, "Layout"),
+		t("({children}: {children: React.ReactNode}){"),
 		t({ "", "    return <main>" }),
 		t("{children}"),
 		t({ "</main>", "}" }),
