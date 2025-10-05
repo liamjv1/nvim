263e78199afccc9bd47d930b99ab4712258b065b
Liam Vinberg <vinberg.liam@gmail.com>
2025-10-05 11:29:39 +0200

    update ai stuff

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,6 +2,7 @@ return {
 	"zbirenbaum/copilot.lua",
 	cmd = "Copilot",
 	event = "InsertEnter",
+    build = ":Copilot auth",
 	config = function()
 		require("copilot").setup({
 			suggestion = {
--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -3,7 +3,7 @@ return {
 	opts = {
 		cli = {
 			mux = {
-				backend = "zellij",
+				backend = "tmux",
 				enabled = true,
 			},
 		},
