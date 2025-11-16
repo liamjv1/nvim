766b17b7d1177bb0bb4eac3ea92a013a6d08e81d
Liam Vinberg <vinberg.liam@gmail.com>
2025-11-16 10:00:27 +0100

    new octo stuff

--- lua/plugins/octo.lua
+++ lua/plugins/octo.lua
@@ -6,10 +6,18 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 	opts = {
-		picker = "telescope",
 		enable_builtin = true,
+		picker = "telescope",
+		default_to_projects_v2 = true,
+		default_merge_method = "squash",
 	},
 	keys = {
 		{ "<leader>O", "<cmd>Octo<cr>", desc = "Octo" },
+		{ "<leader>gi", "<cmd>Octo issue list<CR>", desc = "List Issues (Octo)" },
+		{ "<leader>gI", "<cmd>Octo issue search<CR>", desc = "Search Issues (Octo)" },
+		{ "<leader>gp", "<cmd>Octo pr list<CR>", desc = "List PRs (Octo)" },
+		{ "<leader>gP", "<cmd>Octo pr search<CR>", desc = "Search PRs (Octo)" },
+		{ "<leader>gr", "<cmd>Octo repo list<CR>", desc = "List Repos (Octo)" },
+		{ "<leader>gS", "<cmd>Octo search<CR>", desc = "Search (Octo)" },
 	},
 }
