a5489b75358ae3467c76ad6789cfb123917d711c
Liam Vinberg <vinberg.liam@gmail.com>
2025-12-18 14:35:05 +0100

    remove fff bad

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,8 +1,19 @@
 return {
 	"sindrets/diffview.nvim",
 	dependencies = "nvim-lua/plenary.nvim",
+	cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewFileHistory" }, -- loads when you run these commands
 	keys = {
-		{ "<leader>co", "<cmd>DiffviewOpen<cr>", desc = "Open diffview" },
+		{ "<leader>co", "<cmd>DiffviewOpen<cr>", desc = "Open diffview (index)" },
 		{ "<leader>cc", "<cmd>DiffviewClose<cr>", desc = "Close diffview" },
+		{ "<leader>cd", "<cmd>DiffviewOpen development<cr>", desc = "Diff against development" },
+		{ "<leader>cm", "<cmd>DiffviewOpen main<cr>", desc = "Diff against main" },
+		{ "<leader>ch", "<cmd>DiffviewFileHistory %<cr>", desc = "File history (current file)" },
+	},
+	opts = {
+		view = {
+			default = {
+				layout = "diff2_horizontal",
+			},
+		},
 	},
 }
