a9b32bbfa7e0eb7021c0d7c655b6724d9db7b0af
Liam Vinberg <vinberg.liam@gmail.com>
2025-12-29 18:37:39 +0100

    feat: diffview open all branches and fuzzy find

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -5,8 +5,26 @@ return {
 	keys = {
 		{ "<leader>co", "<cmd>DiffviewOpen<cr>", desc = "Open diffview (index)" },
 		{ "<leader>cc", "<cmd>DiffviewClose<cr>", desc = "Close diffview" },
-		{ "<leader>cd", "<cmd>DiffviewOpen development<cr>", desc = "Diff against development" },
-		{ "<leader>cm", "<cmd>DiffviewOpen main<cr>", desc = "Diff against main" },
+		{
+			"<leader>cb",
+			function()
+				local actions = require("telescope.actions")
+				local action_state = require("telescope.actions.state")
+				require("telescope.builtin").git_branches({
+					attach_mappings = function(prompt_bufnr)
+						actions.select_default:replace(function()
+							actions.close(prompt_bufnr)
+							local selection = action_state.get_selected_entry()
+							if selection then
+								vim.cmd("DiffviewOpen " .. selection.value)
+							end
+						end)
+						return true
+					end,
+				})
+			end,
+			desc = "Diff against branch (pick)",
+		},
 		{ "<leader>ch", "<cmd>DiffviewFileHistory %<cr>", desc = "File history (current file)" },
 	},
 	opts = {
