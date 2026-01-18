3c8f7485709bd5992ae75f1214edc16ead579c80
Liam Vinberg <vinberg.liam@gmail.com>
2026-01-18 16:26:54 +0100

    feat: add opencode to config

---
+++ lua/plugins/opencode.lua
@@ -0,0 +1,33 @@
+return {
+	"NickvanDyke/opencode.nvim",
+	dependencies = {
+		{ "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
+	},
+	config = function()
+		vim.g.opencode_opts = {}
+		vim.o.autoread = true
+		-- Opencode keymaps (no tmux conflicts!)
+		vim.keymap.set({ "n", "x" }, "<leader>oa", function()
+			require("opencode").ask("@this: ", { submit = true })
+		end, { desc = "Ask opencode…" })
+		vim.keymap.set({ "n", "x" }, "<leader>os", function()
+			require("opencode").select()
+		end, { desc = "Execute opencode action…" })
+		vim.keymap.set({ "n", "t" }, "<leader>ot", function()
+			require("opencode").toggle()
+		end, { desc = "Toggle opencode" })
+		vim.keymap.set({ "n", "x" }, "go", function()
+			return require("opencode").operator("@this ")
+		end, { desc = "Add range to opencode", expr = true })
+		vim.keymap.set("n", "goo", function()
+			return require("opencode").operator("@this ") .. "_"
+		end, { desc = "Add line to opencode", expr = true })
+		vim.keymap.set("n", "<leader>ou", function()
+			require("opencode").command("session.half.page.up")
+		end, { desc = "Scroll opencode up" })
+		vim.keymap.set("n", "<leader>od", function()
+			require("opencode").command("session.half.page.down")
+		end, { desc = "Scroll opencode down" })
+	end,
+}
+
